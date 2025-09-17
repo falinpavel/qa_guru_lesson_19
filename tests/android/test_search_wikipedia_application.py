417b7ebae2c7bd091baf1668ea06ea897b021bfe
falinpavel-macbook <aloverapf@yandex.ru>
2025-09-17 22:12:06 +0300

    add local application

--- tests/android/test_search_wikipedia_application.py
+++ tests/android/test_search_wikipedia_application.py
@@ -1,12 +1,17 @@
+import allure
 import pytest
 
 from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
+import const
+
 
 @pytest.mark.parametrize("to_search", ["Appium"])
 def test_search_and_check_result_list(to_search):
+    with allure.step("Skip welcome page"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click()
     with step(f'Tap on search field and type request: {to_search}'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(to_search)
@@ -18,6 +23,8 @@ def test_search_and_check_result_list(to_search):
 
 @pytest.mark.parametrize("to_search", ["Bilbo Baggins", "Frodo Baggins"])
 def test_search_and_go_to_article_page(to_search):
+    with allure.step("Skip welcome page"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click()
     with step(f'Tap on search field and type request: {to_search}'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type(to_search)
