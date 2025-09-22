28a4280542e986b8efabe3d642bfc8f50343fd40
Falin Pavel <falinpavel96@gmail.com>
2025-09-22 17:38:15 +0300

    added config options for local device (samsung s20 fe)

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 
 from appium.options.android import UiAutomator2Options
 
@@ -8,6 +7,7 @@ from const import APK_FILE_PATH
 
 def platform_management(context_platform):
     pass
+
 
 def options_management(context):
     options = UiAutomator2Options()
@@ -22,8 +22,14 @@ def options_management(context):
         options.set_capability(name="EXECUTABLE_PATH", value=os.getenv("EXECUTABLE_PATH"))
 
     if context == 'connected_device':
-        # TODO! dont work`t
-        pytest.mark.xfail(reason="Real devices not supported")
+        options.platform_name = "Android"
+        options.automation_name = "UiAutomator2"
+        options.device_name = "RF8N9193HQY"  # samsung s20 fe
+        options.app = APK_FILE_PATH
+        options.app_wait_activity = "org.wikipedia.*"
+        options.new_command_timeout = 300
+        options.connect_hardware_keyboard = True
+        options.set_capability(name="EXECUTABLE_PATH", value=os.getenv("EXECUTABLE_PATH"))
 
     if context == 'bstack_device':
         options.platform_name = "Android"
@@ -31,12 +37,11 @@ def options_management(context):
         options.app = os.getenv("BS_APP_PATH")
         options.set_capability(name="EXECUTABLE_PATH", value=os.getenv("EXECUTABLE_PATH"))
         options.set_capability(name="bstack:options", value={
-            "projectName": "First Python project", # Название проекта которое будет отображаться в Browserstack
-            "buildName": "browserstack-build-1", # Название сборки которое будет отображаться в Browserstack
-            "sessionName": "BStack first_test", # Название сессии которое будет отображаться в Browserstack
-            # Set your access credentials
-            "userName": os.getenv("BS_LOGIN"), # Ваш логин в Browserstack
-            "accessKey": os.getenv("BS_PASSWORD") # Ваш ключ доступа в Browserstack
+            "projectName": "First Python project",
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+            "userName": os.getenv("BS_LOGIN"),
+            "accessKey": os.getenv("BS_PASSWORD")
         })
 
     return options
